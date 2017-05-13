2348702c95d0f334f44d729286e0d4236027fcb7
Jonas Roncalli <jonas.a.mayer.95@googlemail.com>
2017-05-13 15:38:11 +0200

    Optimization Suggestions - 🅱🅱🅱🅱🅱🅱🅱🅱🅱🅱🅱🅱🅱🅱🅱

--- Task 1 - PAPI Measurements/heatdir/relax_jacobi.c
+++ Task 1 - PAPI Measurements/heatdir/relax_jacobi.c
@@ -14,7 +14,9 @@
 double residual_jacobi(double *u, unsigned sizex, unsigned sizey) {
 	unsigned i, j;
 	double unew, diff, sum = 0.0;
-
+	//idea for optimization: 	go through rows instead of columns
+	//							instead of recomputing unew, compare u to utmp
+	//							vectorization
 	for (j = 1; j < sizex - 1; j++) {
 		for (i = 1; i < sizey - 1; i++) {
 			unew = 0.25 * (u[i * sizex + (j - 1)] +  // left
@@ -35,7 +37,10 @@ double residual_jacobi(double *u, unsigned sizex, unsigned sizey) {
  */
 void relax_jacobi(double *u, double *utmp, unsigned sizex, unsigned sizey) {
 	int i, j;
-
+	//idea for optimization: 	array padding (less conflict misses)
+	//							go through rows instead of columns
+	//							manual vectorization
+	//							loop unrolling
 	for (j = 1; j < sizex - 1; j++) {
 		for (i = 1; i < sizey - 1; i++) {
 			utmp[i * sizex + j] = 0.25 * (u[i * sizex + (j - 1)] +  // left
@@ -46,6 +51,7 @@ void relax_jacobi(double *u, double *utmp, unsigned sizex, unsigned sizey) {
 	}
 
 	// copy from utmp to u
+	//idea for optimization: instead of copying from utmp to u, just swap the pointers
 
 	for (j = 1; j < sizex - 1; j++) {
 		for (i = 1; i < sizey - 1; i++) {
