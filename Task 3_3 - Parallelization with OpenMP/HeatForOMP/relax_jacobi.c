8e93a0ca41f29043bac4fb95f48ae9d15d9a5377
konrad <konrad.proell@gmail.com>
2017-05-26 22:28:01 +0200

    Initial parallelization. About as good as automatic one

--- Task 3_3 - Parallelization with OpenMP/HeatForOMP/relax_jacobi.c
+++ Task 3_3 - Parallelization with OpenMP/HeatForOMP/relax_jacobi.c
@@ -6,7 +6,7 @@
  */
 
 #include "heat.h"
-
+#include "omp.h"
 
 double relax_jacobi( double **u1, double **utmp1,
          unsigned sizex, unsigned sizey )
@@ -17,25 +17,28 @@ double relax_jacobi( double **u1, double **utmp1,
   utmp=*utmp1;
   u=*u1;
   double unew, diff, sum=0.0;
+#pragma omp parallel
 
 
-  for( i=1; i<sizey-1; i++ ) {
-  	int ii=i*sizex;
-  	int iim1=(i-1)*sizex;
-  	int iip1=(i+1)*sizex;
+  {
+#pragma omp for schedule (guided, omp_get_num_threads()) private (unew, diff) reduction (+:sum)
+    for( i=1; i<sizey-1; i++ ) {
+      int ii=i*sizex;
+      int iim1=(i-1)*sizex;
+      int iip1=(i+1)*sizex;
 #pragma ivdep
-    for( j=1; j<sizex-1; j++ ){
-       unew = 0.25 * (u[ ii+(j-1) ]+
-        		            u[ ii+(j+1) ]+
-        		            u[ iim1+j ]+
-        		            u[ iip1+j ]);
-		    diff = unew - u[ii + j];
-		    utmp[ii+j] = unew;
-		    sum += diff * diff;
-
-       }
+      for( j=1; j<sizex-1; j++ ){
+	unew = 0.25 * (u[ ii+(j-1) ]+
+		       u[ ii+(j+1) ]+
+		       u[ iim1+j ]+
+		       u[ iip1+j ]);
+	diff = unew - u[ii + j];
+	utmp[ii+j] = unew;
+	sum += diff * diff;
+	
+      }
     }
-
+  }
   *u1=utmp;
   *utmp1=u;
   return(sum);
