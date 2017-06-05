1636cfea5ed145898d59c3e3da63f9da145da1b6
konrad <konrad.proell@gmail.com>
2017-06-05 11:09:11 +0200

    Finalized Assignments

--- Task 4_2 - Manual implementation of a reduction operation/src/reduction.c
+++ Task 4_2 - Manual implementation of a reduction operation/src/reduction.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include "mpi.h"
 #include <math.h>
+/* Lab Course Efficient Programming of Multicore Processors and Supercomputers
+ * Assignment 4.2: Manual Implementation of a reduction operation
+ * Group 1: Jonas Mayer, Paul Preißner, Konrad Proell
+ */
 
+
+/* We were not quite sure how to understand that the number of processes should only be known at initialization time. We thought it could mean a) that we can only use MPI_Comm_size at initialization time, but reuse the variable, so
+ * that we can not use np==1 as a break condition in our loop, after we updated np during each iteration (we could not do that anyway, because there is no way to synchronize that, and we would have to shut down all processes,
+ * which is pretty ugly. However, from this understanding, we would be allowed to use log2(np) as upper bound for the number of iterations, as this only relies on the number of processes at initialization time.
+ * We think it could also mean, that we can not use any variable related to np as upper bound for the loop. Default behaviour of our code is a), if we are supposed to use b, add #define solutionb
+*/
+
+//#define solutionb
 
 int main (int argc, char** argv)
 {
@@ -20,7 +32,9 @@ int main (int argc, char** argv)
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   /*This forces MPI to not crash on errors, but return errorcodes, which can be used as breakconditions*/
+#ifdef solutionb
   MPI_Errhandler_set(MPI_COMM_WORLD,MPI_ERRORS_RETURN);
+#endif
   int buf=0;
 
   /*calculate the size of the local array*/
@@ -64,12 +78,11 @@ int main (int argc, char** argv)
       sum+=array[ctr];
     }
   ctr=0;
+#ifdef solutionb
   /* This implementation uses to separate break conditions: 1. A node has sent its data to the parentnode 2. The node tries to receive data from a node that does not exist
    * Condition 1. terminates processes {1;np}, while process 0 never sends its data and thus does never meet that condition. However, once process 0 has received the data from process
    * (np/2)-1, and thus the reduction is completed, it tries to receive data from process np, which does not exist. Due to the changed Errorhandler setting, this does not crash the program
-   * but returns a value != MPI_SUCCESS, which can be used as break condition. A probably more readable implementation would include the first break condition, but replace the second by 
-   * checking if there already have been log2(np) iterations. This could easily be realised by for(ctr=0;ctr<int(log2(np));ctr++){//loopbody}. However, the exercise states that the number of 
-   * processes should only be known at initialization time, so we chose this slightly more complicated implementation.
+   * but returns a value != MPI_SUCCESS, which can be used as break condition. However, due to the ugliness of this code, we prefer the default solution.
    */
   while(1)
     {
@@ -80,6 +93,7 @@ int main (int argc, char** argv)
 	  //allows process0 to break out of loop
 	  if (retval!=MPI_SUCCESS)
 	    {
+	      printf("Process %i tried to receive data from process %i, does not exist, thus reduction is finished\n", myid, myid+pow2(ctr));
 	      break;
 	    }
 	  printf("Process %i recvs %i from %i\n", myid, buf ,myid+pow2(ctr));
@@ -94,6 +108,25 @@ int main (int argc, char** argv)
 	} 
       ctr++;
     }
+#else
+  for (ctr=0; ctr<((int) log2(np)); ctr++)
+    {
+      //process recvs
+      if ((myid%(pow2(ctr+1)))==0)
+	{
+	  retval=MPI_Recv(&buf, 1, MPI_INT,  myid+pow2(ctr),MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+	  printf("Process %i recvs %i from %i\n", myid, buf ,myid+pow2(ctr));
+	  sum+=buf;
+	}
+      //process sends      
+      else
+	{
+	  MPI_Send(&sum, 1, MPI_INT,  myid-pow2(ctr),0, MPI_COMM_WORLD);
+	  printf("Process %i sends %i to %i\n", myid, sum,myid-pow2(ctr));
+	  break;
+	} 
+    }
+#endif
   if (myid==0)
     {
       printf("Reduction sum: %d. Correct value is %d.\n", sum, totalsize*(totalsize-1)/2);
