bba15040490bca95c3333ed73094d3ff51f86d3b
Paul Preissner <p001@preissner-muc.de>
2017-06-17 13:26:44 +0200

    added bcasts

--- Task 5 - Parallelization of Heat with MPI/src/heat.c
+++ Task 5 - Parallelization of Heat with MPI/src/heat.c
@@ -30,24 +30,24 @@ int main(int argc, char *argv[]) {
 	algoparam_t param;
 
 	// timing
-
 	double residual;
 
 	// set the visualization resolution
 	param.visres = 100;
 	
-	//MPI shith
+	//MPI vars
 	MPI_Status status;
-	int np, myid;
+	int np, myid, root;
 	MPI_Comm comm;
 	int dim[2], period[2], reorder;
     int coord[2], id;
 	
-	  if ((retval=MPI_Init (&argc, &argv))!=MPI_SUCCESS)
-    fprintf(stderr, "Error initializing MPI, Errorcode %i", retval); 
+	if ((retval=MPI_Init (&argc, &argv))!=MPI_SUCCESS)
+		fprintf(stderr, "Error initializing MPI, Errorcode %i", retval); 
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-	//TODO: adjust dims to useful value from args[]
+	
+	#TODO: adjust dims to useful value from args[]
 	dim[0]= 2; dim[1]=2;
 	//grid -> non periodic
     period[0]=0; period[1]=0;
@@ -63,33 +63,70 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	#TODO:only proc1?{
-	// check input file
-	if (!(infile = fopen(argv[1], "r"))) {
-		fprintf(stderr, "\nError: Cannot open \"%s\" for reading.\n\n", argv[1]);
+	//only proc0 checks file
+	if(myid==0)	{
+		// check input file
+		if (!(infile = fopen(argv[1], "r"))) {
+			fprintf(stderr, "\nError: Cannot open \"%s\" for reading.\n\n", argv[1]);
 
-		usage(argv[0]);
-		return 1;
+			usage(argv[0]);
+			return 1;
+		}
+
+		// check result file
+		resfilename = (argc >= 3) ? argv[2] : "heat.ppm";
+		if (!(resfile = fopen(resfilename, "w"))) {
+			fprintf(stderr, "\nError: Cannot open \"%s\" for writing.\n\n", resfilename);
+
+			usage(argv[0]);
+			return 1;
+		}
+
+		// check input
+		if (!read_input(infile, &param)) {
+			fprintf(stderr, "\nError: Error parsing input file.\n\n");
+
+			usage(argv[0]);
+			return 1;
+		}
+		
+		//broadcast param to all other procs
+	/* REMINDER
+		typedef struct
+		{
+			unsigned maxiter;       // maximum number of iterations
+			unsigned act_res;
+			unsigned max_res;       // spatial resolution
+			unsigned initial_res;
+			unsigned res_step_size;
+			unsigned visres;        // visualization resolution
+		  
+			double *u, *uhelp;
+			double *uvis;
+
+			unsigned   numsrcs;     // number of heat sources
+			heatsrc_t *heatsrcs;
+		}
+		algoparam_t;
+	*/
+		//to avoid performance deterioration due to a single pointer, 
+		// we send a copy of each attribute
+		MPI_Bcast(&(param.maxiter), 1, MPI_UNSIGNED_INT, root, comm);
+		MPI_Bcast(&(param.act_res), 1, MPI_UNSIGNED_INT, root, comm);
+		MPI_Bcast(&(param.max_res), 1, MPI_UNSIGNED_INT, root, comm);
+		MPI_Bcast(&(param.initial_res), 1, MPI_UNSIGNED_INT, root, comm);
+		MPI_Bcast(&(param.res_step_size), 1, MPI_UNSIGNED_INT, root, comm);
+		MPI_Bcast(&(param.visres), 1, MPI_UNSIGNED_INT, root, comm);
+		
+		#TODO: this correct? it's double pointers after all
+		MPI_Bcast(&(param.u), 1, MPI_DOUBLE, root, comm);
+		MPI_Bcast(&(param.uhelp), 1, MPI_DOUBLE, root, comm);
+		MPI_Bcast(&(param.uvis), 1, MPI_DOUBLE, root, comm);
+		
+		MPI_Bcast(&(param.numsrcs), 1, MPI_UNSIGNED_INT, root, comm);
+		MPI_Bcast(&(param.heatsrcs), sizeof(heatsrc_t), MPI_BYTE, root, comm);
 	}
-
-	// check result file
-	resfilename = (argc >= 3) ? argv[2] : "heat.ppm";
-	if (!(resfile = fopen(resfilename, "w"))) {
-		fprintf(stderr, "\nError: Cannot open \"%s\" for writing.\n\n", resfilename);
-
-		usage(argv[0]);
-		return 1;
-	}
-
-	// check input
-	if (!read_input(infile, &param)) {
-		fprintf(stderr, "\nError: Error parsing input file.\n\n");
-
-		usage(argv[0]);
-		return 1;
-	}
-	#TODO: broadcast param to all other procs}
-	#TODO: all other procs receive param and save
+	
 	print_params(&param);
 	time = (double *) calloc(sizeof(double), (int) (param.max_res - param.initial_res + param.res_step_size) / param.res_step_size);
 
@@ -119,18 +156,22 @@ int main(int argc, char *argv[]) {
 			#TODO: give chunksize and chunkoffset
 			residual = relax_jacobi(&(param.u), &(param.uhelp), np, np);
 			#TODO: send borders to neighbours
+			#TODO: receive borders from neighbors
+			//potential deadlock here?
 		}
 		#TODO: gather residual
 		t1 = gettime();
 		time[exp_number] = wtime() - time[exp_number];
 
-		printf("\n\nResolution: %u\n", param.act_res);
-		printf("===================\n");
-		printf("Execution time: %f\n", time[exp_number]);
-		printf("Residual: %f\n\n", residual);
+		if(myid == 0) {
+			printf("\n\nResolution: %u\n", param.act_res);
+			printf("===================\n");
+			printf("Execution time: %f\n", time[exp_number]);
+			printf("Residual: %f\n\n", residual);
 
-		printf("megaflops:  %.1lf\n", (double) param.maxiter * (np - 2) * (np - 2) * 7 / time[exp_number] / 1000000);
-		printf("  flop instructions (M):  %.3lf\n", (double) param.maxiter * (np - 2) * (np - 2) * 7 / 1000000);
+			printf("megaflops:  %.1lf\n", (double) param.maxiter * (np - 2) * (np - 2) * 7 / time[exp_number] / 1000000);
+			printf("  flop instructions (M):  %.3lf\n", (double) param.maxiter * (np - 2) * (np - 2) * 7 / 1000000);
+		}
 
 		exp_number++;
 	}
@@ -138,8 +179,15 @@ int main(int argc, char *argv[]) {
 	param.act_res = param.act_res - param.res_step_size;
 
 	coarsen(param.u, param.act_res + 2, param.act_res + 2, param.uvis, param.visres + 2, param.visres + 2);
-	#TODO:gather image
-	write_image(resfile, param.uvis, param.visres + 2, param.visres + 2);
+	
+	//gather image
+	if(myid != 0) {
+		#TODO: all non-root processes send their image parts to p0?
+	} else {
+		#TODO: recv image parts, stitch into one?
+		
+		write_image(resfile, param.uvis, param.visres + 2, param.visres + 2);
+	}
 
 	finalize(&param);
 	MPI_Finalize();
