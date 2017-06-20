f92f086f1feb7483ef5fad004a9e6806208c6de2
konrad <konrad.proell@gmail.com>
2017-06-21 00:13:43 +0200

    Fixed interprocess communication

--- Task 5 - Parallelization of Heat with MPI/src/heat.c
+++ Task 5 - Parallelization of Heat with MPI/src/heat.c
@@ -57,7 +57,6 @@ int main(int argc, char *argv[]) {
 		usage(argv[0]);
 		return 1;
 	}
-	//if (myid==root)
 	// check input file
 	int file_free=0;
 	{
@@ -103,15 +102,24 @@ int main(int argc, char *argv[]) {
 	MPI_Cart_coords(comm_2d, myid, 2, coords);
 	param.row=coords[0];
 	param.col=coords[1];
-
 	if (myid == root)
 		print_params(&param);
 	time = (double *) calloc(sizeof(double), (int) (param.max_res - param.initial_res + param.res_step_size) / param.res_step_size);
 
 	int source, north, south, east, west;
-	MPI_Cart_shift(comm_2d, 0, 1, &west, &east);
-	MPI_Cart_shift(comm_2d, 1, 1, &north, &south);
-	
+	MPI_Cart_shift(comm_2d, 0, 1, &north, &south);
+	MPI_Cart_shift(comm_2d, 1, 1, &west, &east);
+	// hack, do not do this at home!
+	/*	int tmpdim=north;
+	north=west;
+	west=south;
+	south=east;
+	east=tmpdim;
+	tmpdim=west;
+	west=east;
+	east=tmpdim;
+	tmpdim=0;
+	*/
 	int exp_number = 0;
 
 	for (param.act_res = param.initial_res; param.act_res <= param.max_res; param.act_res = param.act_res + param.res_step_size) {
@@ -120,8 +128,9 @@ int main(int argc, char *argv[]) {
 
 			usage(argv[0]);
 		}
-		
-		//---DEBUG ONLY
+
+
+		/*	//---DEBUG ONLY, FOR TESTING OF INIT VALUES
 		file_free=0;
 		if (myid==root)
 		  file_free=1;
@@ -149,6 +158,7 @@ int main(int argc, char *argv[]) {
 		      MPI_Send (&file_free, 1, MPI_INT, myid+1, 1, MPI_COMM_WORLD);
 		  }
 		//---DEBUG ONLY
+		*/
 		
 		
 		// changed from act_res
@@ -163,30 +173,7 @@ int main(int argc, char *argv[]) {
 		residual = 999999999;
 		globresid = residual;
 		np = param.act_res + 2;
-		
-
-		
-
-
-
-
-		
-		int tileSizeX=((np-2)/dim[0])+2;
-		int tileSizeY=((np-2)/dim[1])+2;
-		int tileOffsetX = coords[0]*(tileSizeX-2);
-		int tileOffsetY = coords[1]*(tileSizeY-2);
-		//printf("Proc %d: NP: %d, TilesizeX: %d, TileOffsetX: %d, Last Element: %d \n",myid,np,tileSizeX, tileOffsetX,tileOffsetX+tileSizeX-1);
-		
-		if(coords[0]==dim[0]-1)
-		{
-			tileSizeX+=(np-2)%dim[0];
-					}
-		if(coords[1]==dim[1]-1)
-		{
-			tileSizeY+=(np-2)%dim[1];
-		//	printf("Proc %d: NP: %d, TilesizeY: %d, TileOffsetY: %d, Last Element: %d \n",myid,np,tileSizeY, tileOffsetY,tileOffsetY+tileSizeY-1);
-		}
-		
+			
 		int npx = param.arraysize_x + 2;
 		int npy = param.arraysize_y + 2;
 		
@@ -195,24 +182,91 @@ int main(int argc, char *argv[]) {
 		MPI_Type_commit(&north_south_type);
 		// create east-west type
 		MPI_Datatype east_west_type;
-		MPI_Type_vector(param.arraysize_y,1,npx,MPI_DOUBLE, &east_west_type);
+		MPI_Type_vector(npy-2,1,npx,MPI_DOUBLE, &east_west_type);
 		MPI_Type_commit(&east_west_type);
 
 		t0 = gettime();
 		for (iter = 0; iter < param.maxiter; iter++) {
 		  residual = relax_jacobi(&(param.u), &(param.uhelp), npx, npy, param.len_x, param.len_y);
-			
+		  // prints array after 1 iteration and communication, debug
+		  if (iter==0)
+		    
+		    {file_free=0;
+		      if (myid==root)
+			file_free=1;
+		      else
+			MPI_Recv(&file_free, 1, MPI_INT, myid-1, 1, MPI_COMM_WORLD, &status);
+		      if (file_free=1)
+			{
+			  if(param.act_res * param.act_res < 200) {
+			    fprintf(stderr,"\np%d: my partial array is\n",myid);
+			    for (i = 0; i < param.arraysize_y + 2; i++) {
+			      if(i==1)
+				fprintf(stderr,"---------\n");
+			      for (j = 0; j < param.arraysize_x + 2; j++) {
+				if(j==param.arraysize_x+1 || j==1)
+				  fprintf(stderr,"| ");
+				fprintf(stderr,"%f ", param.u[i * (param.arraysize_x + 2) + j]);
+			      }
+			      fprintf(stderr,"\n");
+			      if(i==param.arraysize_y)
+			  fprintf(stderr,"---------\n");
+			    }
+			    fprintf(stderr,"\n\n");
+			  }
+			  if (myid!=nprocs-1)
+			    MPI_Send (&file_free, 1, MPI_INT, myid+1, 1, MPI_COMM_WORLD);
+			}}
 		  
+		  MPI_Barrier(MPI_COMM_WORLD);
 			MPI_Request reqs[8];
+
 			MPI_Isend(&param.u[1+npx] , 1, north_south_type, north, 9, comm_2d, &reqs[0]);
+			
 			MPI_Isend(&param.u[1+param.arraysize_y*npx] , 1, north_south_type, south, 9, comm_2d, &reqs[1]);
-			MPI_Isend(&param.u[1+npx], 1, east_west_type, west, 9, comm_2d, &reqs[2]);
-			MPI_Isend(&param.u[param.arraysize_y+npx], 1, east_west_type, east, 9, comm_2d, &reqs[3]);
-			MPI_Irecv(&param.u[1], 1, north_south_type, north, 9, comm_2d, &reqs[4]);
-			MPI_Irecv(&param.u[1+(param.arraysize_y)*npx+npx], 1, north_south_type, south, 9, comm_2d, &reqs[5]);
-			MPI_Irecv(&param.u[1*npx], 1, east_west_type, east, 9, comm_2d, &reqs[6]);
-			MPI_Irecv(&param.u[param.arraysize_x+1+npx], 1, east_west_type, west, 9, comm_2d, &reqs[7]);
+			
+			MPI_Irecv(&param.u[1], 1, north_south_type, north, 9, comm_2d, &reqs[2]);
+
+			MPI_Irecv(&param.u[1+(param.arraysize_y)*npx+npx], 1, north_south_type, south, 9, comm_2d, &reqs[3]);
+
+			MPI_Isend(&param.u[1+npx], 1, east_west_type, west, 9, comm_2d, &reqs[4]);
+			// buggy			
+			MPI_Isend(&param.u[2*npx-2], 1, east_west_type, east, 9, comm_2d, &reqs[5]);
+			
+			MPI_Irecv(&param.u[2*npx-1], 1, east_west_type, east, 9, comm_2d, &reqs[6]);
+			
+			MPI_Irecv(&param.u[npx], 1, east_west_type, west, 9, comm_2d, &reqs[7]);
+			
 			MPI_Waitall(8, reqs, MPI_STATUS_IGNORE);
+			
+			if (iter==0)
+		    
+		    {file_free=0;
+		      if (myid==root)
+			file_free=1;
+		      else
+			MPI_Recv(&file_free, 1, MPI_INT, myid-1, 1, MPI_COMM_WORLD, &status);
+		      if (file_free=1)
+			{
+			  if(param.act_res * param.act_res < 200) {
+			    fprintf(stderr,"\np%d: my partial array is\n",myid);
+			    for (i = 0; i < param.arraysize_y + 2; i++) {
+			      if(i==1)
+				fprintf(stderr,"---------\n");
+			      for (j = 0; j < param.arraysize_x + 2; j++) {
+				if(j==param.arraysize_x+1 || j==1)
+				  fprintf(stderr,"| ");
+				fprintf(stderr,"%f ", param.u[i * (param.arraysize_x + 2) + j]);
+			      }
+			      fprintf(stderr,"\n");
+			      if(i==param.arraysize_y)
+				fprintf(stderr,"---------\n");
+			    }
+			    fprintf(stderr,"\n\n");
+			  }
+			  if (myid!=nprocs-1)
+			    MPI_Send (&file_free, 1, MPI_INT, myid+1, 1, MPI_COMM_WORLD);
+			}}
 		  
 			/*	
 				the residual used to be a condition to break. because we use allreduce, all processes have the correct residual and this
@@ -237,6 +291,8 @@ int main(int argc, char *argv[]) {
 			printf("megaflops:  %.1lf\n", (double) param.maxiter * (np - 2) * (np - 2) * 7 / time[exp_number] / 1000000);
 			printf("  flop instructions (M):  %.3lf\n", (double) param.maxiter * (np - 2) * (np - 2) * 7 / 1000000);
 		}
+	
+
 		exp_number++;
 	}
 	// change to ==root, if this should work
--- Task 5 - Parallelization of Heat with MPI/src/relax_jacobi.c
+++ Task 5 - Parallelization of Heat with MPI/src/relax_jacobi.c
@@ -23,7 +23,7 @@ double relax_jacobi( double **u1, double **utmp1,
   	int ii=i*sizex;
   	int iim1=(i-1)*sizex;
   	int iip1=(i+1)*sizex;*/
-   for( i=1; i<len_y-1; i++ ) {
+   for( i=1; i<len_y+1; i++ ) {
   	int ii=i*sizex;
   	int iim1=(i-1)*sizex;
   	int iip1=(i+1)*sizex;
@@ -32,7 +32,7 @@ double relax_jacobi( double **u1, double **utmp1,
 
 #pragma ivdep
 	//TODO: j = chunkoffset_x, i<chunkoffset_x+chunksize_x-1 && i<sizex-1
-    for( j=1; j<len_x-1; j++ ){
+    for( j=1; j<len_x+1; j++ ){
       //  for( j=ofx+1; j<ofx+sizex-1; j++ ){
        unew = 0.25 * (u[ ii+(j-1) ]+
         		            u[ ii+(j+1) ]+
